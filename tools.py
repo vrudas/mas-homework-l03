cea8a6cd124d4787eb8418163021f67f6a4cd2b9
vrudas <vrudas4work@gmail.com>
2026-03-18 03:01:05 +0200

    Implemented writing of result file function

--- tools.py
+++ tools.py
@@ -1,5 +1,18 @@
+import os
+
+from config import settings
+
+
 def write_report(filename: str, content: str) -> str:
-    pass
+    path = "./" + settings.output_dir + "/" + filename
+
+    dirpath = os.path.dirname(path) or "."
+    os.makedirs(dirpath, exist_ok=True)
+
+    with open(path, 'w', encoding="utf-8") as f:
+        f.write(content)
+
+    return f"Report written to {path}"
 
 
 def web_search(query: str) -> list[dict]:
