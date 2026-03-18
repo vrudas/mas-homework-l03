779446162cc09558a79bf3de23665b23d6ebf3e3
vrudas <vrudas4work@gmail.com>
2026-03-18 03:03:14 +0200

    Implemented fetching of web_search results

--- tools.py
+++ tools.py
@@ -1,5 +1,7 @@
 import os
 
+from ddgs import DDGS
+
 from config import settings
 
 
@@ -16,8 +18,19 @@ def write_report(filename: str, content: str) -> str:
 
 
 def web_search(query: str) -> list[dict]:
-    pass
-
+    try:
+        search_results = DDGS().text(query, max_results=settings.max_search_results)
+        return [
+            {
+                "title": search_result.get("title"),
+                "url": search_result.get("href"),
+                "snippet": search_result.get("body"),
+            }
+            for search_result in search_results
+        ]
+    except Exception:
+        print(f"Error fetching URL: {query}")
+        return []
 
 def read_url(url: str) -> str:
     pass
